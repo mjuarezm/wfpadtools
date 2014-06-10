1616cc8eded40752c0ce13fa0243670cde490f88
mjuarezm <marcjuarez89@gmail.com>
2014-06-10 12:53:01 +0200

    Test whether obfsproxy is found in bin.

--- obfsproxy/test/transports/wfpadtools/runenv_test.py
+++ obfsproxy/test/transports/wfpadtools/runenv_test.py
@@ -1,3 +1,4 @@
+from os.path import join, exists
 import unittest
 
 from obfsproxy.test.transports.wfpadtools.sttest import STTest
@@ -15,6 +16,16 @@ class RunEnvTest(STTest):
     def test_requesocks(self):
         self.assert_package_is_installed('requesocks')
 
+    def test_bin_obfsproxy(self):
+        """Checks obfsproxy executable/link is found in bin.
+        
+        This is necessary to run tester.py. We assume unix dir structure
+        as it also is assumed in tester.py. Since we want to use pyobfsproxy
+        to run our transports still under development, we can create a symbolic
+        link to obfsproxy's python version running the following command: 
+        sudo ln -s ~/git/obfsproxy-wfpadtools/obfsproxy/pyobfsproxy.py /bin/obfsproxy
+        """
+        self.assertTrue(exists(join("/bin", "obfsproxy")))
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
