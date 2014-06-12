465fa3cc5d442929b01c6e17abaf26bdb74f2009
mjuarezm <marcjuarez89@gmail.com>
2014-06-12 20:12:35 +0200

    Add method to create messages individually and encapsulate methods related to creation of messages into a class.

--- obfsproxy/transports/wfpadtools/message.py
+++ obfsproxy/transports/wfpadtools/message.py
@@ -1,77 +1,21 @@
-"""This module provides code to handle WFPad protocol messages.
+"""This module provides methods to handle WFPad protocol messages.
 
 The exported classes and functions provide interfaces to handle protocol
 messages, check message headers for validity and create protocol messages out
-of application data. It also filters padding messages out of the stream.
+of application data.
 
-This module is basically the same as ScrambleSuit's but modified to our
-protocol specification.
+This module is basically the same as ScrambleSuit's message but modified to
+our protocol specification.
 """
 import obfsproxy.common.log as logging
 import obfsproxy.common.serialize as pack
 import obfsproxy.transports.base as base
 from obfsproxy.transports.wfpadtools import const
+from obfsproxy.transports.scramblesuit import probdist
+import random
 
 
 log = logging.get_obfslogger()
-
-
-def createWFPadMessages(data, flags=const.FLAG_DATA):
-    """Create protocol messages out of the given payload.
-
-    The given `data` is turned into a list of protocol messages with the given
-    `flags` set.  The list is then returned.
-    """
-    messages = []
-
-    while len(data) > const.MPU:
-        messages.append(WFPadMessage(data[:const.MPU], flags=flags))
-        data = data[const.MPU:]
-
-    messages.append(WFPadMessage(data, flags=flags))
-
-    log.debug("Created %d protocol messages." % len(messages))
-
-    return messages
-
-
-def getFlagNames(flags):
-    """Return the flag name encoded in the integer `flags` as string.
-
-    This function is only useful for printing easy-to-read flag names in debug
-    log messages.
-    """
-    if flags == const.FLAG_DATA:
-        return "DATA"
-    elif flags == const.FLAG_PADDING:
-        return "PADDING"
-    else:
-        return "Undefined"
-
-
-def isSane(totalLen, payloadLen, flags):
-    """Verifies whether the given header fields are sane.
-
-    The values of the fields `totalLen`, `payloadLen` and `flags` are checked
-    for their sanity.  If they are in the expected range, `True` is returned.
-    If any of these fields has an invalid value, `False` is returned.
-    """
-    def isFine(length):
-        """Check if the given length is fine."""
-        return True if (0 <= length <= const.MPU) else False
-
-    log.debug("Message header: totalLen=%d, payloadLen=%d, flags"
-              "=%s" % (totalLen, payloadLen, getFlagNames(flags)))
-
-    validFlags = [
-        const.FLAG_DATA,
-        const.FLAG_PADDING
-    ]
-
-    return isFine(totalLen) and \
-           isFine(payloadLen) and \
-           totalLen >= payloadLen and \
-           (flags in validFlags)
 
 
 class WFPadMessage(object):
@@ -99,6 +43,90 @@ class WFPadMessage(object):
     def __len__(self):
         """Return the length of this protocol message."""
         return const.HDR_LENGTH + self.totalLen
+
+
+class WFPadMessageFactory(object):
+    """Creates WFPad's protocol messages."""
+
+    def __init__(self, len_dist=None):
+        """Initialize the WFPadMessageFactory with the distribution `len_dist`.
+
+        If `len_dist` is `None`, a new discrete probability distribution is
+        generated randomly.
+        """
+        if len_dist:
+            self.len_dist = len_dist
+        else:
+            self.len_dist = probdist.new(lambda: random.randint(
+                                                            const.HDR_LENGTH,
+                                                            const.MTU))
+
+    def set_len_distribution(self, new_len_dist):
+        """Set a new length probability distribution."""
+        self.len_dist = new_len_dist
+
+    def createWFPadMessage(self, payload="", paddingLen=0,
+                           flags=const.FLAG_DATA):
+        return WFPadMessage(payload, paddingLen, flags)
+
+    def createWFPadMessages(self, data, flags=const.FLAG_DATA):
+        """Create protocol messages out of the given payload.
+
+        The given `data` is turned into a list of protocol messages with the
+        given `flags` set. The list is then returned.
+        """
+        messages = []
+        while len(data) > 0:
+            msg_len = self.len_dist.randomSample()
+            data_len = len(data)
+            if data_len > msg_len:
+                messages.append(self.createWFPadMessage(data[:msg_len]))
+                data = data[msg_len:]
+            else:
+                messages.append(self.createWFPadMessage(data[:data_len],
+                                             paddingLen=msg_len - data_len))
+                data = data[data_len:]
+        log.debug("Created %d protocol messages." % len(messages))
+        return messages
+
+
+def getFlagNames(flags):
+    """Return the flag name encoded in the integer `flags` as string.
+
+    This function is only useful for printing easy-to-read flag names in
+    debug log messages.
+    """
+    if flags == const.FLAG_DATA:
+        return "DATA"
+    elif flags == const.FLAG_PADDING:
+        return "PADDING"
+    else:
+        return "Undefined"
+
+
+def isSane(totalLen, payloadLen, flags):
+    """Verifies whether the given header fields are sane.
+
+    The values of the fields `totalLen`, `payloadLen` and `flags` are
+    checked for their sanity.  If they are in the expected range, `True` is
+    returned. If any of these fields has an invalid value, return `False`.
+    """
+    def isFine(length):
+        """Check if the given length is fine."""
+        return True if (0 <= length <= const.MPU) else False
+
+    log.debug("Message header: totalLen=%d, payloadLen=%d, flags"
+              "=%s" % (totalLen, payloadLen, getFlagNames(flags)))
+
+    validFlags = [
+        const.FLAG_DATA,
+        const.FLAG_PADDING
+    ]
+
+    return isFine(totalLen) and \
+           isFine(payloadLen) and \
+           totalLen >= payloadLen and \
+           (flags in validFlags)
 
 
 class WFPadMessageExtractor(object):
