d7054193008b72d892bb801470d06e14f3bfc6fb
mjuarez <marcjuarez89@gmail.com>
2014-08-21 20:13:28 +0200

    Rollback probdist to commit 5cb3367.

--- obfsproxy/transports/scramblesuit/probdist.py
+++ obfsproxy/transports/scramblesuit/probdist.py
@@ -20,7 +20,7 @@ class RandProbDist:
     Provides code to generate, sample and dump probability distributions.
     """
 
-    def __init__( self, genSingleton, genProbSignleton, seed=None ):
+    def __init__( self, genSingleton, genProbSignleton=None, seed=None ):
         """
         Initialise a discrete probability distribution.
 
@@ -45,6 +45,7 @@ class RandProbDist:
         returns the probabilities for the singletons returned by
         `genSingleton`.
         """
+
         dist = {}
 
         # Amount of distinct bins, i.e., packet lengths or inter arrival times.
@@ -54,8 +55,10 @@ class RandProbDist:
         cumulProb = 0
 
         for index in xrange(bins):
-            #prob = self.prng.uniform(0, (1 - cumulProb))
-            prob = genProbSignleton(index, bins, cumulProb)
+            if genProbSignleton:
+                prob = genProbSignleton(index, bins, cumulProb)
+            else:
+                prob = self.prng.uniform(0, (1 - cumulProb))
             cumulProb += prob
 
             singleton = genSingleton()
@@ -96,9 +99,10 @@ class RandProbDist:
 
         return self.sampleList[-1][1]
 
+
 # Alias class name in order to provide a more intuitive API.
 new = RandProbDist
 
 
 def uniform(x):
-    return new(lambda i, n, c: x, lambda i, n, c: 1)
+    return new(lambda i, n, c: x, lambda: 1)
