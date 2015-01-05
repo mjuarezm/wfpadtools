4b34d646dcd932fe4c811cfa38e0123c1781210e
mjuarezm <marcjuarez89@gmail.com>
2015-01-05 18:16:48 +0100

    Changes in wfpadtools:
    * Add template for new csbuflo test.
    * Implement "payload padding" primitive for the new spec of CS-BuFLO

---
+++ obfsproxy/test/transports/wfpadtools/specific/csbuflo_test.py
@@ -0,0 +1,20 @@
+import unittest
+
+
+class Test(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_payload_padding(self):
+        pass
+
+    def test_total_padding(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
