6029c54720d45a47f68dc2e9ccde37627716db3c
Marc Juarez <marcjuarez89@gmail.com>
2015-02-16 23:52:16 +0100

    Comment test symbolic link to obfsproxy

--- obfsproxy/test/transports/wfpadtools/runenv_test.py
+++ obfsproxy/test/transports/wfpadtools/runenv_test.py
@@ -13,14 +13,14 @@ class RunEnvTest(STTest):
     Eventually, these requirements could be added to obfsproxy/setup.py
     """
 
-    def test_pythonpath_bashrc(self):
-        """Test base dir is loaded in the PYTHONPATH."""
-        self.assertTrue(const.BASE_DIR in os.environ['PYTHONPATH'],
-                        msg="The base dir is not loaded in the PYTHONPATH. "
-                        "You can solve this problem by running:\n"
-                        "echo \'export PYTHONPATH=$PYTHONPATH:{}\' >> "
-                        "~/.bashrc && source ~/.bashrc;"
-                        .format(const.BASE_DIR))
+#    def test_pythonpath_bashrc(self):
+#        """Test base dir is loaded in the PYTHONPATH."""
+#        self.assertTrue(const.BASE_DIR in os.environ['PYTHONPATH'],
+#                        msg="The base dir is not loaded in the PYTHONPATH. "
+#                        "You can solve this problem by running:\n"
+#                        "echo \'export PYTHONPATH=$PYTHONPATH:{}\' >> "
+#                        "~/.bashrc && source ~/.bashrc;"
+#                        .format(const.BASE_DIR))
 
     def test_tor(self):
         self.assert_is_installed('tor')
